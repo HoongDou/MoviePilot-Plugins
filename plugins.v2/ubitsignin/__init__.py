a2d074548af103626b400f296ec7e2f61c79083c
HoongDou <921120271a@sina.com>
2026-06-23 01:20:39 +0800

    Update __init__.py

--- plugins.v2/ubitsignin/__init__.py
+++ plugins.v2/ubitsignin/__init__.py
@@ -23,7 +23,7 @@ class UBitsSignIn(_PluginBase):
     # 插件图标，建议放在 plugins.v2/ubitsignin/signin.png
     plugin_icon = "signin.png"
     # 插件版本
-    plugin_version = "1.2.0"
+    plugin_version = "1.3.0"
     # 插件作者
     plugin_author = "HoongDou"
     # 作者主页
